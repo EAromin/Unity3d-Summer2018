19c497b087a99d99396a12359c6373fb6f56fa1d
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-05-14 14:50:12 -0400

    Rope physics done

---
+++ Cut The Rope Clone/Assets/Scripts/Rope.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody2D hook;
+
+	public GameObject linkPrefab;
+	public Weight endWeight;
+
+	public int linkNum = 8;
+	void Start () {
+		GenerateRope ();
+	}
+
+	void GenerateRope(){
+
+		Rigidbody2D previousLink = hook;
+		for (int i = 0; i < linkNum; i++) {
+			GameObject currentLink = Instantiate (linkPrefab, transform);
+			HingeJoint2D joint = currentLink.GetComponent<HingeJoint2D> ();
+			joint.connectedBody = previousLink;
+			previousLink = currentLink.GetComponent<Rigidbody2D>();
+
+		}
+
+
+		endWeight.SetRopeEnd (previousLink.GetComponent <Rigidbody2D>());
+	}
+
+}
---
+++ Cut The Rope Clone/Assets/Scripts/Weight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Weight : MonoBehaviour {
+	public float distFromChain = .5f;
+	public void SetRopeEnd(Rigidbody2D end){
+		HingeJoint2D joint = gameObject.AddComponent <HingeJoint2D>();
+		joint.autoConfigureConnectedAnchor = false;
+		joint.connectedBody = end;
+		joint.anchor = Vector2.zero;
+		joint.connectedAnchor = new Vector2 (0f,-distFromChain);
+	}
+}
