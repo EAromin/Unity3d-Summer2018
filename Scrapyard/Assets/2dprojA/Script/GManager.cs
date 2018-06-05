9b959cf2de1aca3a22a3b69b49cc1706aa897b18
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-06-05 19:52:36 -0400

    addedLineRenderer

---
+++ Scrapyard/Assets/2dprojA/Script/GManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GManager : MonoBehaviour {
+
+	bool gameEnd = false;
+	public float delay = 1f;
+	// Use this for initialization
+	public void EndGame () {
+		if(!gameEnd)
+		{
+			gameEnd = true;
+			Debug.Log("Game over");
+			Invoke("Restart",delay);
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Restart () {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
--- Scrapyard/Assets/2dprojA/Script/PlayerCollider.cs
+++ Scrapyard/Assets/2dprojA/Script/PlayerCollider.cs
@@ -9,6 +9,13 @@ public class PlayerCollider : MonoBehaviour {
 	// Update is called once per frame
 	void OnCollisionEnter2D(Collision2D coll){
 		pc.grounded = true;
+
+		if(coll.collider.tag == "Spike"){
+			pc.enabled = false;
+			FindObjectOfType<GManager>().EndGame();
+			Destroy(this.gameObject);
+
+		}
 	}
 	void OnCollisionExit2D(Collision2D coll){
 		
