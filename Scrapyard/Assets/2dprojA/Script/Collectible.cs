47316ff6261a380fbe39b391607e31564cf1f08d
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-06-07 01:56:50 -0400

    Added some sprites and other visual stuff.

---
+++ Scrapyard/Assets/2dprojA/Script/Collectible.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+	// Use this for initialization
+	public float offset = .05f;
+	float bottomLimit,topLimit;
+	public float animSpeed = 1.0f;
+	Vector2 target;
+	public Rigidbody2D rb;
+	private bool movingUp = true;
+	void Start () {
+		
+		topLimit = rb.transform.position.y+ offset;
+		bottomLimit = rb.transform.position.y-offset;
+	}	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(rb.transform.position.y > topLimit)
+			movingUp =false;
+		else if(rb.transform.position.y < bottomLimit)
+			movingUp = true;
+		if(movingUp)
+			rb.velocity = Vector2.MoveTowards(rb.transform.position,Vector2.up,10f) * animSpeed;
+		else
+			rb.velocity =  Vector2.MoveTowards(rb.transform.position,Vector2.up,10f) * -animSpeed;
+
+		rb.transform.Rotate(Vector2.up,Space.World);
+		Debug.Log(movingUp);
+	}
+}
