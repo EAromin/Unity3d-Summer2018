ca797ea9d988a28ed95df71762870d4d645279e8
Earl <earlaromin@gmail.com>
2018-06-11 19:55:00 -0400

    yes

--- Scrapyard/Assets/2dprojA/Script/GManager.cs
+++ Scrapyard/Assets/2dprojA/Script/GManager.cs
@@ -5,13 +5,7 @@ public class GManager : MonoBehaviour {
 
 	bool gameEnd = false;
 	public float delay = 1f;
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public GameObject levelCompleteUI;
-=======
->>>>>>> 47316ff6261a380fbe39b391607e31564cf1f08d
-=======
->>>>>>> 47316ff6261a380fbe39b391607e31564cf1f08d
 	// Use this for initialization
 	public void EndGame () {
 		if(!gameEnd)
@@ -27,14 +21,8 @@ public class GManager : MonoBehaviour {
 	void Restart () {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 	void CompleteLevel(){
 
 	}
-=======
->>>>>>> 47316ff6261a380fbe39b391607e31564cf1f08d
-=======
->>>>>>> 47316ff6261a380fbe39b391607e31564cf1f08d
 }
--- Scrapyard/Assets/2dprojA/Script/Rope.cs
+++ Scrapyard/Assets/2dprojA/Script/Rope.cs
@@ -38,13 +38,7 @@ public class Rope : MonoBehaviour {
 		for(int i =0; i < links.Length; i++)
 		{	if(links[i]!=null)
 			lr.SetPosition(i,links[i].transform.position);
-<<<<<<< HEAD
-<<<<<<< HEAD
 			else break;
-=======
->>>>>>> 47316ff6261a380fbe39b391607e31564cf1f08d
-=======
->>>>>>> 47316ff6261a380fbe39b391607e31564cf1f08d
 		}
 	}
 	void GenerateLinks(){
