7f63aa6763674ecd1fc48a6ff6e56e939644c6c0
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-07-21 17:06:48 -0400

    Horizontal Rotation

--- 3D-Summer-Project/Assets/Scripts/CameraController.cs
+++ 3D-Summer-Project/Assets/Scripts/CameraController.cs
@@ -13,15 +13,21 @@ public class CameraController : MonoBehaviour
 	public float minZoom = 5f;
 	public float maxZoom = 15f;
 
+	public float yawRate = 100f;
+	private float yawInput = 0f;
 	void Update(){
 		zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomRate;
 		zoom = Mathf.Clamp(zoom,minZoom,maxZoom);
+
+		yawInput -= Input.GetAxis("Horizontal") * yawRate * Time.deltaTime;
 	}
     void LateUpdate()
     {
 		//Acts just like Update, once per turn, but is called after update
 		transform.position = target.position - offset * zoom;
 		transform.LookAt(target.position+ Vector3.up *pitch);
+
+		transform.RotateAround(target.position,Vector3.up,yawInput);
 
     }
 }
