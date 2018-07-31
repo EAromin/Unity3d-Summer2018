60a55b086629327a7e13e1c4689b3aed4f871192
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-07-31 19:47:03 -0400

    Adds character graphics
    Adds model with clothes
    Adds animations

---
+++ 3D-Summer-Project/Assets/Scripts/CharAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class CharAnim : MonoBehaviour {
+
+	// Use this for initialization
+	Animator animator;
+	NavMeshAgent agent;
+	const float locomotionAnimationSmoothTime = .1f;
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat("speedPercent",speedPercent,locomotionAnimationSmoothTime,Time.deltaTime); 
+		Debug.Log(speedPercent);
+	}
+}
